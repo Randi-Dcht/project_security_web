0a6d011af96e10a2f4d7f9008faa70bbca0370c8
Guillaume Kerckhofs <guillaume.kerckhofs@student.umons.ac.be>
2023-05-10 16:06:38 +0200

    add some pages

---
+++ frontend/src/pages/patient/Profil.jsx
@@ -0,0 +1,14 @@
+
+
+const Profil = () =>
+{
+
+    return(
+        <div>
+            <h3 className="title is-2">Mon profil :</h3>
+            
+        </div>
+    )
+}
+
+export default Profil;
