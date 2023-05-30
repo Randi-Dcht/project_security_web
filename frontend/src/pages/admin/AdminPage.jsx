9ec2d5f137c778e2dc2bd3845d9d8b1b15d7315a
Guillaume Kerckhofs <guillaume.kerckhofs@student.umons.ac.be>
2023-05-30 18:09:35 +0200

    add buttons on Admin and Doctor profil

--- frontend/src/pages/admin/AdminPage.jsx
+++ frontend/src/pages/admin/AdminPage.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import '../../styles/DoctorPage.css';
 
 const AdminPage = () => {
   const [users, setUsers] = useState(['User 1', 'User 2', 'User 3']);
 
+  const handleSecurityUpdate = () => {
+      // TODO : logique pour modifier les informations lié à la sécurité de l'utilisateur
+  };
   const handleApproveUser = (user) => {
     // Approuver l'utilisateur
     console.log(`Approuver ${user}`);
@@ -14,17 +18,19 @@ const AdminPage = () => {
   };
 
   return (
-    <div>
+    <div className="doctor-page">
       <h1>Page de l'Admin</h1>
-
-      <h2>Tous les utilisateurs</h2>
-      <ul>
-        {users.map((user, index) => (
-          <li key={index}>
-            {user} <button onClick={() => handleApproveUser(user)}>Approuver</button> <button onClick={() => handleDeleteUser(user)}>Supprimer</button>
-          </li>
-        ))}
-      </ul>
+        <button className="red_btn" onClick={handleSecurityUpdate}>Modifier la sécurité</button>
+        <section>
+          <h2>Tous les utilisateurs</h2>
+          <ul>
+            {users.map((user, index) => (
+              <li key={index}>
+                {user} <button onClick={() => handleApproveUser(user)}>Approuver</button> <button onClick={() => handleDeleteUser(user)}>Supprimer</button>
+              </li>
+            ))}
+          </ul>
+        </section>
     </div>
   );
 };
