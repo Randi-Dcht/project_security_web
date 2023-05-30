e37bf66a6a7503afd5c97411b05fe801599c319f
Guillaume Kerckhofs <guillaume.kerckhofs@student.umons.ac.be>
2023-05-30 17:21:14 +0200

    button modify information + security for user

--- frontend/src/pages/patient/UserPage.jsx
+++ frontend/src/pages/patient/UserPage.jsx
@@ -6,6 +6,12 @@ const UserPage = () => {
   const [records, setRecords] = useState(["Record 1", "Record 2"]);
   const [file, setFile] = useState(null);
 
+  const handleUserUpdate = () => {
+    // TODO : logique pour modifier les informations de l'utilisateur
+  };
+  const handleSecurityUpdate = () => {
+      // TODO : logique pour modifier les informations lié à la sécurité de l'utilisateur
+  };
   const handleDoctorAdd = () => {
     // TODO : logique pour ajouter un médecin
   };
@@ -28,6 +34,11 @@ const UserPage = () => {
 
   return (
     <div className="user-page">
+      <section>
+        <h2>Mes informations</h2>
+        <button onClick={handleUserUpdate}>Modifier mes informations</button>
+        <button class="red_btn" onClick={handleSecurityUpdate}>Modifier la sécurité</button>
+      </section>
       <section>
         <h2>Médecins</h2>
         <ul>
