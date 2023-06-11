98fe353beb907b5b6ed75a9acc2f4c8fe3289179
Samain Clément <clement-samain@hotmail.com>
2023-06-11 12:22:10 +0200

    update userInfo page

--- frontend/src/pages/UserInfo.jsx
+++ frontend/src/pages/UserInfo.jsx
@@ -6,14 +6,66 @@ const DoctorPage = () => {
   const [lastName, setLastName] = useState('');
   const [mail, setMail] = useState('');
   const [isEditing, setIsEditing] = useState(false);
+  const [oldName, setOldName] = useState('');
+  const [oldLastName, setOldLastName] = useState('');
+  const [oldMail, setOldMail] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://localhost:1026/me', {
+            method: 'GET',
+            credentials: 'include'
+        });
+        
+        const data = await response.json();
+  
+        console.log(data);
+  
+        if (data) {
+          setName(data.name);
+          setLastName(data.lastName);
+          setMail(data.email);
+        }
+  
+      } catch (error) {
+        console.error('Erreur lors de la requête HTTPS:', error);
+        window.location.href = '/error';
+      }
+    };
+  
+    fetchData();
+  }, []);
 
   const handleEdit = () => {
+    setOldLastName(lastName);
+    setOldName(name);
+    setOldMail(mail);
     setIsEditing(!isEditing);
   };
 
   const handleSubmit = () => {
-    // Ici, vous pouvez faire une requête API pour mettre à jour les informations sur le serveur.
-    console.log(name, lastName, mail);
+    if (name === '') {
+      setName(oldName);
+    }
+    if (lastName === '') {
+      setLastName(oldLastName);
+    }
+    if (mail === '') {
+      setMail(oldMail);
+    }
+    if (name === '' && lastName === '' && mail === '') {
+      return;
+    }
+    if (mail !== oldMail) {
+      //TODO changer le mail et le p12
+    }
+    if (name !== oldName ) {
+      //TODO changer le nom
+    }
+    if (lastName !== oldLastName) {
+      //TODO changer le prenom
+    }
     setIsEditing(false);
   };
 
@@ -21,6 +73,7 @@ const DoctorPage = () => {
     <div className="doctor-page">
       {isEditing ? (
         <div>
+          <p>Modification des informations (laisser vide pour ne pas modifier)</p>
           <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="Nom"/>
           <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="Prénom"/>
           <input type="text" value={mail} onChange={e => setMail(e.target.value)} placeholder="Email"/>
